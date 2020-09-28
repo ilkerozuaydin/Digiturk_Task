9d02b890cedac12f5829157adf759254fd4ae196
ilkerozuaydin <71990116+ilkerozuaydin@users.noreply.github.com>
2020-09-28 15:15:30 +0300

    Data Access Layer Create

---
+++ DataAccess/Concrete/EntityFramework/Context/BlogContext.cs
@@ -0,0 +1,38 @@
+﻿
+using Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Reflection;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class BlogContext : DbContext
+    {
+        private readonly string _connectionString;
+        public BlogContext()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            _connectionString = configuration.GetConnectionString("BlogDbConnection");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+
+}
---
+++ DataAccess/DependencyResolvers/DataAccessModule.cs
@@ -0,0 +1,20 @@
+﻿using Core.Abstract.DependencyResolvers;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.DependencyResolvers
+{
+    public class DataAccessModule : IDependencyResolverModule
+    {
+        public void Load(IServiceCollection services)
+        {
+            services.AddScoped<IUserDal, EfUserDal>();
+            services.AddScoped<IArticleDal, EfArticleDal>();
+            services.AddScoped<ICommentDal, EfCommentDal>();
+        }
+    }
+}
